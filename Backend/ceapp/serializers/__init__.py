9e5427b25d8bc1bd10c568645cde069c3570a643
Nicolas Gomez Martinez (Alumno) <nicolas.gomezma@usm.cl>
2025-08-18 12:22:05 -0400

    Cambio en __init__.py

--- Backend/ceapp/serializers/__init__.py
+++ Backend/ceapp/serializers/__init__.py
@@ -1,5 +1,3 @@
-# ceapp/serializers/__init__.py
-
 from .auth import CustomTokenObtainPairSerializer, RegisterSerializer
 from .plan_trabajo import PlanTrabajoSerializer
 from .objetivos import (
@@ -16,11 +14,7 @@ from .relaciones import (
     BitacoraEntradaObjetivoSerializer,
 )
 
-# --- ALIAS DE COMPATIBILIDAD PARA LAS VIEWS ACTUALES ---
-# Lo que antes se llamaba así:
 ObjetivoSerializer = ObjetivoReadSerializer
 ObjetivoPOSTSerializer = ObjetivoWriteSerializer
-# (Si alguna view usa este nombre “antiguo”, seguirá resolviendo)
-# Si alguna view usa BitacoraEntradaSerializer y falla en POST,
-# cambia este alias a BitacoraEntradaWriteSerializer o ajusta esa view.
+
 BitacoraEntradaSerializer = BitacoraEntradaReadSerializer
